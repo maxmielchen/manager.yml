4a6ba63f039fb748ec93a38f6e3cc2fd8ad623ae
maxmielchen <max@mielchen.email>
2024-03-09 14:45:05 +0100

    Update Traefik configuration for dashboard and API

--- init.sh
+++ init.sh
@@ -14,17 +14,19 @@ echo
 
 ACME_EMAIL=
 PORTAINER_DOMAIN=
-DASHBOARD_DOMAIN=
-DASHBOARD_USER=
-DASHBOARD_PASSWORD=
+TRAEFIK_DASHBOARD_DOMAIN=
+TRAEFIK_API_DOMAIN=
+TRAEFIK_USER=
+TRAEFIK_PASSWORD=
 read -p "Email (for Let's Encrypt): " ACME_EMAIL
 read -p "Portainer domain: " PORTAINER_DOMAIN
-read -p "Dashboard domain: " DASHBOARD_DOMAIN
-read -p "Dashboard user: " DASHBOARD_USER
-read -s -p "Dashboard password: " DASHBOARD_PASSWORD
+read -p "Treafik dashboard domain: " TRAEFIK_DASHBOARD_DOMAIN
+read -p "Treafik API domain: " TRAEFIK_API_DOMAIN
+read -p "Treafik user: " TRAEFIK_USER
+read -s -p "Treafik password: " TRAEFIK_PASSWORD
 echo
-DASHBOARD_LOGIN=$(htpasswd -nbB -C 8 $DASHBOARD_USER $DASHBOARD_PASSWORD | sed -e s/\\$/\\$\\$/g)
-echo "Dashboard login: $DASHBOARD_LOGIN"
+TRAEFIK_LOGIN=$(htpasswd -nbB -C 8 $TRAEFIK_USER $TRAEFIK_PASSWORD | sed -e s/\\$/\\$\\$/g)
+echo "Traefik login: $TRAEFIK_LOGIN"
 
 
 echo
@@ -40,15 +42,19 @@ cp template_manager.yml manager.yml
 if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i "" "s#{{ACME_EMAIL}}#$ACME_EMAIL#g" manager.yml
     sed -i "" "s@{{PORTAINER_DOMAIN}}@$PORTAINER_DOMAIN@g" manager.yml
-    sed -i "" "s@{{DASHBOARD_DOMAIN}}@$DASHBOARD_DOMAIN@g" manager.yml
-    sed -i "" "s#{{DASHBOARD_USER}}#$DASHBOARD_USER#g" manager.yml
-    sed -i "" "s@{{DASHBOARD_LOGIN}}@$DASHBOARD_LOGIN@g" manager.yml
+    sed -i "" "s@{{TRAEFIK_DASHBOARD_DOMAIN}}@$TRAEFIK_DASHBOARD_DOMAIN@g" manager.yml
+    sed -i "" "s@{{TRAEFIK_API_DOMAIN}}@$TRAEFIK_API_DOMAIN@g" manager.yml
+    sed -i "" "s@{{TRAEFIK_USER}}@$TRAEFIK_USER@g" manager.yml
+    sed -i "" "s@{{TRAEFIK_PASSWORD}}@$TRAEFIK_PASSWORD@g" manager.yml
+    sed -i "" "s@{{TRAEFIK_LOGIN}}@$TRAEFIK_LOGIN@g" manager.yml
 else
     sed -i "s#{{ACME_EMAIL}}#$ACME_EMAIL#g" manager.yml
     sed -i "s@{{PORTAINER_DOMAIN}}@$PORTAINER_DOMAIN@g" manager.yml
-    sed -i "s@{{DASHBOARD_DOMAIN}}@$DASHBOARD_DOMAIN@g" manager.yml
-    sed -i "s#{{DASHBOARD_USER}}#$DASHBOARD_USER#g" manager.yml
-    sed -i "s@{{DASHBOARD_LOGIN}}@$DASHBOARD_LOGIN@g" manager.yml
+    sed -i "s@{{TRAEFIK_DASHBOARD_DOMAIN}}@$TRAEFIK_DASHBOARD_DOMAIN@g" manager.yml
+    sed -i "s@{{TRAEFIK_API_DOMAIN}}@$TRAEFIK_API_DOMAIN@g" manager.yml
+    sed -i "s@{{TRAEFIK_USER}}@$TRAEFIK_USER@g" manager.yml
+    sed -i "s@{{TRAEFIK_PASSWORD}}@$TRAEFIK_PASSWORD@g" manager.yml
+    sed -i "s@{{TRAEFIK_LOGIN}}@$TRAEFIK_LOGIN@g" manager.yml
 fi
 echo "manager.yml created..."
 
@@ -61,6 +67,6 @@ echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
 fi
-docker network create proxy || exit 1
+docker network create proxy
 docker compose -f manager.yml -p manager up -d  || exit 1
 echo "Manager started..."
